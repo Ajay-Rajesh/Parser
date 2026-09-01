9feb38156f11e1dc9bc22c29f57f0cce164aea64
Ajay-Rajesh <rajeshajay224@gmail.com>
2026-09-02 00:34:42 +0530

    added voice files and tts

--- backend/parser/chat.py
+++ backend/parser/chat.py
@@ -1,6 +1,6 @@
 from retrieve import retrieve
 from generate import generate_answer
-# from speech import speak
+from speech import speak
 
 while True:
     q = input("\nAsk: ")
@@ -14,7 +14,7 @@ while True:
 
     print("\nAnswer:\n")
     print(answer)
-# for text to speech
-    # lang = "ta" if any("\u0B80" <= c <= "\u0BFF" for c in answer) else "en"
 
-    # speak(answer, lang)
+    lang = "ta" if any("\u0B80" <= c <= "\u0BFF" for c in answer) else "en"
+
+    speak(answer, lang)
--- backend/parser/speech.py
+++ backend/parser/speech.py
@@ -1,12 +1,24 @@
-# from piper import PiperVoice
-# import sounddevice as sd
+from pathlib import Path
+from piper import PiperVoice
+import sounddevice as sd
+import numpy as np
 
-# ta = PiperVoice.load("backend/voices/ta_IN-medium.onnx")
-# en = PiperVoice.load("backend/voices/en_US-medium.onnx")
+VOICE_DIR = Path(__file__).resolve().parent.parent / "Voices"
 
-# def speak(text, lang="ta"):
-#     voice = ta if lang == "ta" else en
+ta = PiperVoice.load(str(VOICE_DIR / "ta_IN-roja-medium.onnx"))
+en = PiperVoice.load(str(VOICE_DIR / "en_GB-alan-medium.onnx"))
 
-#     audio = voice.synthesize(text)
-#     sd.play(audio.audio_float_array, audio.sample_rate)
-#     sd.wait()
+def speak(text, lang="ta"):
+    voice = ta if lang == "ta" else en
+
+    audio = []
+
+    # synthesize() yields chunks
+    for chunk in voice.synthesize(text):
+        audio.extend(chunk.audio_int16_bytes)
+
+    audio = np.frombuffer(bytes(audio), dtype=np.int16)
+    audio = audio.astype(np.float32) / 32768.0
+
+    sd.play(audio, 22050)
+    sd.wait()
